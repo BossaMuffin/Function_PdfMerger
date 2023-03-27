f2cef7a38160f4cdcdf047703180271f07ab859e
BossaMuffin <105994546+BossaMuffin@users.noreply.github.com>
2023-03-27 12:55:52 +0200

    v2 - alphabetical ordered files
    - The files to merge are alphabeticaly ordered ;
    - Fix the deprecated method of PyPDF2 (PdfFileMerge to PdfMerger).

--- main.py
+++ main.py
@@ -4,15 +4,26 @@
 import PyPDF2
 import os
 
+#g_merger = PyPDF2.PdfFileMerger()
+g_merger = PyPDF2.PdfMerger()
 
-g_merger = PyPDF2.PdfFileMerger()
 
-def start():
-    for l_file in os.listdir(os.curdir):
+def get_files():
+    # Get the files to merge, alphabetical ordered
+    list_dir = os.listdir('.')
+    list_dir_lower = [f.lower() for f in list_dir]   # Convert to lower case
+    return sorted(list_dir)
+
+
+def start(list_dir):
+    for l_file in list_dir:
         if l_file.endswith(".pdf") or l_file.endswith(".PDF"):
             g_merger.append(l_file)
     g_merger.write("combined.pdf")
 
 
 if __name__ == '__main__':
-    start()
+
+    start(get_files())
+
+    
